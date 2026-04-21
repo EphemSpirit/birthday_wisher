eaf04610cfe1c0d0938263e65256a18c8cca3e3f
Drew Hund <languageinvestigator@gmail.com>
2026-04-21 17:41:38 -0400

    send birhtday emails

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import os
 import smtplib
 from dotenv import load_dotenv
+import pandas
+import datetime as dt
+import random
 
 load_dotenv()
 
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(user=os.getenv("GMAIL_ADDRESS"), password=os.getenv("GMAIL_APP_PASSWORD"))
-    connection.sendmail(
-        from_addr=os.getenv("GMAIL_ADDRESS"),
-        to_addrs=os.getenv("YAHOO_ADDRESS"),
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+today = (dt.datetime.now().month, dt.datetime.now().day)
+
+birthday_data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(row.month, row.day):row for (_, row) in birthday_data.iterrows()}
+
+def send_birthday_email():
+    birthday_haver = birthday_dict[today]
+    files = os.listdir("letter_templates")
+
+    with open(f"letter_templates/{random.choice(files)}") as file:
+        data = file.read()
+        data = data.replace("[NAME]", birthday_haver["name"])
+
+    with smtplib.SMTP(os.getenv("SMTP_HOST"), 587) as connection:
+        connection.starttls()
+        connection.login(user=os.getenv("GMAIL_ADDRESS"), password=os.getenv("GMAIL_APP_PASSWORD"))
+        connection.sendmail(
+            from_addr=os.getenv("GMAIL_ADDRESS"),
+            to_addrs=os.getenv("YAHOO_ADDRESS"),
+            msg=f"Subject:Happy Birthday!\n\n{data}."
+        )
+
+if today in birthday_dict:
+    send_birthday_email()
+
+
+
